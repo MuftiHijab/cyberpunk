b8c66220ab2109e28d3118f2ad2f52414ba0acc0
MuftiHijab <99564022+MuftiHijab@users.noreply.github.com>
2022-03-03 19:36:30 -0500

    Add files via upload
    Hopefully it worked!

---
+++ app2.js
@@ -0,0 +1,14 @@
+var acc = document.getElementsByclassName ('accordion');
+var i;
+var len = acc.length; 
+for (i = 0; i < len; i++) {
+    acc[i].addEventListener ('click', function() {
+        this.classList.toggle('active');
+        var panel = this.nextElementSibling;
+        if(panel.style.maxHeight) {
+             panel.style.maxHeight = null;
+         } else {
+             panel.style.maxHeight = panel.scrollHeight + 'px'
+         }
+    })
+}
